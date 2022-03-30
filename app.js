6b5cee910f7c5eeae24210df570fbd6efb8eb67c
KateM3d <ekaterina8medvedeva@gmail.com>
2022-03-29 20:26:50 -0400

    tuple

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 //object
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
 var person = {
+    // const person = {
     name: "John",
     age: 30,
-    hobbies: ["sports", "cooking"]
+    hobbies: ["sports", "cooking"],
+    role: [2, "author"]
 };
+person.role.push("admin");
+person.role[0] = 10;
 var favoriteActivities;
 // let favoriteActivities: any[]; losing the ts benefits, ut allows us to use all types of data inside the array.
 favoriteActivities = ["sports"];
-console.log(person.name);
+console.log(person);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
--- app.ts
+++ app.ts
@@ -1,21 +1,27 @@
 //object
 
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  // const person = {
   name: "John",
   age: 30,
   hobbies: ["sports", "cooking"],
+  role: [2, "author"],
 };
+
+person.role.push("admin");
+person.role[0] = 10;
 
 let favoriteActivities: string[];
 // let favoriteActivities: any[]; losing the ts benefits, ut allows us to use all types of data inside the array.
 
 favoriteActivities = ["sports"];
 
-console.log(person.name);
+console.log(person);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
